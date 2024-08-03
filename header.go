1bcf7a6cbe2aa9150443c7a8e7115d64c624dcb1
David Palmer <dp88@users.noreply.github.com>
2024-08-03 16:21:43 -0400

    just read those bytes right into the struct

--- header.go
+++ header.go
@@ -1,10 +1,13 @@
 package qoi
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 )
 
 type header struct {
+	magic      [4]byte
 	width      uint32
 	height     uint32
 	channels   uint8
@@ -14,23 +17,19 @@ type header struct {
 func readHeader(data [14]byte) (header, error) {
 	var h header
 
-	// Check for the magic string
-	if string(data[:4]) != "qoif" {
+	if err := binary.Read(bytes.NewReader(data[:]), binary.BigEndian, &h); err != nil {
+		return h, err
+	}
+
+	if string(h.magic[:]) != "qoif" { // Check for the magic string
 		return h, errors.New("qoi: invalid magic string")
 	}
 
-	// Read the width and height
-	h.width = uint32(data[4])<<24 | uint32(data[5])<<16 | uint32(data[6])<<8 | uint32(data[7])
-	h.height = uint32(data[8])<<24 | uint32(data[9])<<16 | uint32(data[10])<<8 | uint32(data[11])
-
-	if h.width == 0 || h.height == 0 {
+	if h.width == 0 || h.height == 0 { // Check for valid dimensions
 		return h, errors.New("qoi: invalid image dimensions")
 	}
 
-	h.channels = data[12]
-	h.colorspace = data[13]
-
-	if h.channels != 3 && h.channels != 4 {
+	if h.channels != 3 && h.channels != 4 { // Check for valid number of channels
 		return h, errors.New("qoi: invalid number of channels")
 	}
 
